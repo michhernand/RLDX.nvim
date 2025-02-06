bb9d777eb4cda9ac2f815f1ab41949514f3f9831
michael <mhernan88@gmail.com>
2025-02-06 00:38:04 -0600

    starting from scratch

--- main.go
+++ main.go
@@ -1,28 +1,40 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
-	"github.com/urfave/cli/v3"
-
-	"dex/lib/nvim"
-	dbcmd "dex/app/cmd/api"
+	"github.com/neovim/go-client/nvim"
 )
 
+func hello(v *nvim.Nvim, args []string) error {
+	return v.WriteOut(fmt.Sprintf("Hello %s\n", strings.Join(args, " ")))
+}
 
 func main() {
-	cmd := &cli.Command{
-		Commands: []*cli.Command{
-			dbcmd.Cmd,
-		},
-		Action: nvim.StartService,
+	// Turn off timestamps in output.
+	log.SetFlags(0)
+
+	// Direct writes by the application to stdout garble the RPC stream.
+	// Redirect the application's direct use of stdout to stderr.
+	stdout := os.Stdout
+	os.Stdout = os.Stderr
+
+	// Create a client connected to stdio. Configure the client to use the
+	// standard log package for logging.
+	v, err := nvim.New(os.Stdin, stdout, stdout, log.Printf)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	ctx := context.Background()
-	err := cmd.Run(ctx, os.Args)
-	if err != nil {
+	// Register function with the client.
+	v.RegisterHandler("hello", hello)
+
+	// Run the RPC message loop. The Serve function returns when
+	// nvim closes.
+	if err := v.Serve(); err != nil {
 		log.Fatal(err)
 	}
 }
