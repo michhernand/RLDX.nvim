c5545f0a759662eecce035e4b1b7020a6df7e341
michael <mhernan88@gmail.com>
2025-02-06 12:26:10 -0600

    added case sensitive flag

--- lua/rolodex/config.lua
+++ lua/rolodex/config.lua
@@ -5,39 +5,86 @@ M.options = {}
 M._options = nil
 
 local defaults = {
-	binary_path = "/Users/michael/dev/rolodex.nvim/dex"
+	prefix_char = "@",
+	binary_path = "/Users/michael/dev/rolodex.nvim/dex",
+	case_sensitive = true,
 }
 
+local chan = nil
+
 function M.ensure_job(path)
+	if chan then
+		return chan
+	end
+
 	chan = vim.fn.jobstart({ path }, { rpc = true })
 	return chan
 end
 
-function M.setup(options)
-	print("SETTING UP")
-	M._options = options
+function M.detect_prefix(prefix_char, case_sensitive)
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
 
-	if options == nil then
-		error("NIL")
+	if not line then 
+		return {
+			false,
+			"",
+			case_sensitive
+		}
 	end
-	M.options = vim.tbl_deep_extend("force", {}, defaults, M._options or {})
+
+	local before_cursor = line:sub(1, col)
+	local after_cursor = line:sub(col + 1)
+
+	local word_start = before_cursor:match("(%S+)$") or ""
+	local word_end = after_cursor:match("^(%S*)") or ""
+
+	local word = word_start .. word_end
+	return {
+		(word:sub(1, 1) == prefix_char), 
+		word,
+		case_sensitive,
+	}
+end
+
+function M.setup(options)
+	M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
 
 	if M.options.binary_path == nil then
-		error("binary_path not populated in opts")
+		vim.notify("binary_path not populated in opts", "error")
 	end
 
 	if M.options.binary_path == "" then
-		error("binary_path is empty string in opts")
+		vim.notify("binary_path is empty string in opts", "error")
 	end
 
-	vim.notify(M.options.binary_path)
+	if M.options.prefix_char == nil then
+		vim.notify("prefix_char is not populated in opts", "error")
+	end
+
+	if M.options.prefix_char == "" then
+		vim.notify("prefix_char is empty string in opts", "error")
+	end
 
 	vim.api.nvim_create_user_command('DexLookup', function(args)
-		vim.fn.rpcrequest(
-		-- require("rolodex").ensure_job(M.options.binary_path), 
+		local prefix_args = M.detect_prefix(
+			M.options.prefix_char, 
+			M.options.case_sensitive
+		)
+
+		if prefix_args[1] == false then
+			vim.notify("Terminating Early")
+			return
+		end
+
+		local result = vim.fn.rpcrequest(
 		M.ensure_job(M.options.binary_path), 
-		'dex_lookup', args.fargs
-	)
+		'dex_lookup', prefix_args
+		)
+
+		if result ~= nil then
+			vim.notify(vim.inspect(result))
+		end
 
 	end, { nargs = '*' })
 end
