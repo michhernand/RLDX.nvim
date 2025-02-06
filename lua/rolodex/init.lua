ff767c8906a6cc118ea62113b76e029035d7c8ba
michael <mhernan88@gmail.com>
2025-02-06 01:25:35 -0600

    removing unused stuff

--- lua/rolodex/init.lua
+++ lua/rolodex/init.lua
@@ -1,21 +1,11 @@
 local M = {}
 
-function M.autocomplete_handler()
-	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-	local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
-
-	if not line then return end
-
-	local before_cursor = line:sub(1, col)
-	local after_cursor = line:sub(col + 1)
-
-	local word_start = before_cursor:match("(%S+)$") or ""
-	local word_end = after_cursor:match("^(%S*)") or ""
-
-	local word = word_start .. word_end
-	if word:sub(1, 1) == "@" then
-		vim.notify("has prefix -> " .. word)
+function M.ensure_job(path)
+	if chan then
+		return chan
 	end
+	chan = vim.fn.jobstart({ path }, { rpc = true })
+	return chan
 end
 
 return M
