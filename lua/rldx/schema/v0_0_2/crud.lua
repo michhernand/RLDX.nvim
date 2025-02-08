7412413273bc759b8d7c34d0d521c925fab7024d
michael <mhernan88@gmail.com>
2025-02-07 23:45:02 -0600

    adding schema v002 funcs

---
+++ lua/rldx/schema/v0_0_2/crud.lua
@@ -0,0 +1,27 @@
+local utils = require("rldx.utils")
+local middleware = require("rldx.schema.v0_0_2.middleware")
+
+local M = {}
+
+function M.save_contacts(filepath, catalog)
+	local ok, err = utils.write_json_file(
+		filepath,
+		catalog
+	)
+	return ok, err
+end
+
+function M.load_contacts(filepath, create)
+	local catalog, err = utils.read_json_file(filepath, create)
+	if err ~= nil then
+		return nil, err
+	end
+	return middleware.to_completions(catalog), nil
+end
+
+function M.add_contact(filepath, name, catalog)
+	table.insert(catalog, { name = name, count= -1 })
+	return M.save_contacts(filepath, catalog)
+end
+
+return M
---
+++ lua/rldx/schema/v0_0_2/middleware.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+function M.to_completions(data)
+	local output = {}
+	for _, entry in ipairs(data) do
+		table.insert(
+			output,
+			{
+				label = entry.name,
+				kind = cmp.lsp.CompletionItemKind.Text,
+			}
+		)
+	end
+	return output
+end
+
+function M.from_completions(data)
+	local output = {}
+	for _, entry in ipairs(data) do
+		table.insert(
+			output,
+			{
+				name = entry.label,
+				count = -1,
+			}
+		)
+	end
+	return output
+end
