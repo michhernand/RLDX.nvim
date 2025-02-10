391db10db36b170ee12430418a8de3f9dc1fcdb8
michael <mhernan88@gmail.com>
2025-02-10 11:50:37 -0600

    add deep copy func

--- lua/rldx/utils/algos.lua
+++ lua/rldx/utils/algos.lua
@@ -21,6 +21,16 @@ function M.dedupe(data)
 	return res
 end
 
+function M.copy_table(obj, seen)
+	if type(obj) ~= 'table' then return obj end
+	if seen and seen[obj] then return seen[obj] end
+	local s = seen or {}
+	local res = setmetatable({}, getmetatable(obj))
+	s[obj] = res
+	for k, v in pairs(obj) do res[M.copy_table(k, s)] = M.copy_table(v, s) end
+	return res
+end
+
 return M
 
 
